c559cc0898876b0ade6de59ce3995e92857bd615
嘉禾 <jiahe.zjh@dtyunxi.com>
2023-10-24 14:51:32 +0800

    update

--- src/views/Creation/CreateProblem/Create.tsx
+++ src/views/Creation/CreateProblem/Create.tsx
@@ -28,6 +28,7 @@ import { useNavigate, useSearchParams } from 'react-router-dom'
 import Dragger from 'antd/es/upload/Dragger'
 import { createTagApi, createTagAutoApi } from '@/api/tag'
 import { RcFile, UploadChangeParam } from 'antd/es/upload'
+import { UploadProblemModal } from '@/components/Problem/UploadProblem/UploadProblemModal'
 
 const stringArrItem = [
   'test_input',
@@ -54,8 +55,6 @@ const Create: React.FC = () => {
   const [currentStep, setcurrentStep] = useState(0)
   const [failMessage, setfailMessage] = useState()
   const [loading, setloading] = useState(false)
-  const [fileList, setFileList] = useState<UploadFile[]>([])
-  const [failUploadFileList, setFailUploadFileList] = useState<UploadFile[]>([])
   const [uploadDone, setUploadDone] = useState(false)
   const [currentProblem, setCurrentProblem] =
     useRecoilState(currentProblemState)
@@ -250,43 +249,7 @@ const Create: React.FC = () => {
       .catch(err => console.log(err))
   }, [])
 
-  const handleUpload = useCallback(
-    (file: UploadFile) => {
-      setFailUploadFileList([])
-      if (!file.originFileObj) return
-      file.originFileObj.text().then(text => {
-        const data = JSON.parse(text)
-        // const index = data.time_limit.search(/[A-z]|[a-z]/)
-        // data.time_unit = data.time_limit.slice(index).toLowerCase()
-        // data.time_limit = Number(data.time_limit.slice(0, index))
-        // const index1 = data.memory_limit.search(/[A-Z]|[a-z]/)
-        // data.memory_unit = data.memory_limit.slice(index1).toLowerCase()
-        // data.memory_unit.includes('b')
-        //   ? null
-        //   : (data.memory_unit = `${data.memory_unit}b`)
-        // data.memory_limit = Number(data.memory_limit.slice(0, index1))
-        // data.oj = 'POJ'
-        data.problem_id = String(data.id)
-        data.sample_case = [
-          {
-            input: data.sample_case.sample_input,
-            output: data.sample_case.sample_outpit
-          }
-        ]
-        delete data.id
-        uploadVjudgeProblemApi(JSON.stringify(data))
-          .then(res => {
-            console.log(res.data)
-          })
-          .catch(err => {
-            console.log(err)
-            file.status = 'error'
-            setFailUploadFileList(value => [...value, file])
-          })
-      })
-    },
-    [failUploadFileList]
-  )
+
 
   return (
     <div className="flex">
@@ -419,77 +382,8 @@ const Create: React.FC = () => {
         </div>
       </div>
 
-      <Modal
-        title={'上传题目文件'}
-        open={openUploadModal}
-        onCancel={() => setOpenUploadModal(false)}
-        footer={
-          [
-            // <Button
-            //   type="primary"
-            //   disabled={fileList.length === 0}
-            //   onClick={handleUpload}
-            // >
-            //   点击上传
-            // </Button>
-          ]
-        }
-        style={{
-          translate: '0 50%'
-        }}
-      >
-        <Space>
-          <Dragger
-            accept="text/xml"
-            directory
-            name="file"
-            multiple={true}
-            // action={'http://api_oj.mgaronya.com/problem/create/by/file'}
-            // headers={{
-            //   Authorization: localStorage.getItem('token') as string,
-            //   'Content-Type': 'multipart/form-data'
-            // }}
-            beforeUpload={file => {
-              console.log(file)
-              const form = new FormData()
-              form.append('file', file)
-              uploadProblemByFileApi(form).then(res => {
-                console.log(res)
-              })
-            }}
-          >
-            <div className="p-2">
-              点击或拖拽批量上传.xml文件(支持上传文件夹)
-            </div>
-          </Dragger>
-          <Dragger
-            accept="application/json"
-            name="file"
-            multiple={true}
-            // beforeUpload={(file, fileList) => {
-            //   setFileList(fileList)
-            //   return false
-            // }}
-            onRemove={file => {
-              const index = failUploadFileList.findIndex(
-                value => value.uid === file.uid
-              )
-              const newFileList = failUploadFileList.slice()
-              newFileList.splice(index, 1)
-              setFailUploadFileList(newFileList)
-            }}
-            onChange={(info: UploadChangeParam<UploadFile<RcFile>>) => {
-              console.log(info.file.name)
-              handleUpload(info.file)
-            }}
-            fileList={failUploadFileList}
-          >
-            <div className="p-2">
-              点击批量上传外站题目.json文件(支持上传文件夹)
-            </div>
-          </Dragger>
-        </Space>
-      </Modal>
+      <UploadProblemModal openUploadModal={openUploadModal} setOpenUploadModal={setOpenUploadModal}></UploadProblemModal>
+
       <Modal
         title="自动创建标签"
         open={openCreateTagModal}
