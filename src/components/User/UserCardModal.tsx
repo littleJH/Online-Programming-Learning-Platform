12ba067b960b819234a5de69cc9bd5c1b4ee7f5d
zhangjiahao <2240046361@qq.com>
2023-11-10 12:20:38 +0800

    update

---
+++ src/components/User/UserCardModal.tsx
@@ -0,0 +1,9 @@
+import { User } from '@/type'
+import React from 'react'
+
+const UserCardDetail: React.FC<{ user: User }> = (props) => {
+  const { user } = props
+  return <div></div>
+}
+
+export default UserCardDetail
